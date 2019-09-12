fc3e2704992a70d171bd452ea2f38f76d5694b09
Stef Spears <spears.sj87@gmail.com>
2019-09-12 03:23:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,5 +7,6 @@ def key_for_min_value(name_hash)
   smallest_value = 0 
   smallest_key = nil 
   name_hash.each do |key, val|
-    if val <
+    if val < smallest_value || smallest_value == 0 
+      smallest_value = val 
 end
