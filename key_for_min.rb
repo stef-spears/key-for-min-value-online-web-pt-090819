f47a588ce9ecb9f567816f8030fd18a410e556f9
Stef Spears <spears.sj87@gmail.com>
2019-09-12 03:22:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,5 +4,8 @@ require 'pry'
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  
+  smallest_value = 0 
+  smallest_key = nil 
+  name_hash.each do |key, val|
+    if val <
 end
